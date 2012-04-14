6816f64ccc58351184ddb724513c368316492895
Grayson Koonce <grayson.koonce@gmail.com>
2012-04-14 13:49:37 -0700

    fixing autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,4 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-$autoload = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php';
-if (is_file($autoload)) {
-    require_once $autoload;
-} else {
-    require_once $autoload . '.dist';
-}
+require __DIR__ . '/../vendor/.composer/autoload.php';
