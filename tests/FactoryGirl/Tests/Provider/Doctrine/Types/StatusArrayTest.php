26f28e33ba72b40e47ce786ea9e8522dc4aa379f
Peter Strøiman <peter@stroiman.com>
2015-06-10 09:18:03 +0200

    Copied MockPlatform from Doctrine sources
    The tests for this project had a dependency to mocks defined in
    Doctrine test code. This is unfortunate, as this is not part of the
    public api and can easily be changed, which it has.
    
    Therefore has the class been copied to this code base. As the Doctrine
    is currently licensed under the MIT license, the direct copying of that
    code should not be a problem and in violation of any licensing issues.

--- tests/FactoryGirl/Tests/Provider/Doctrine/Types/StatusArrayTest.php
+++ tests/FactoryGirl/Tests/Provider/Doctrine/Types/StatusArrayTest.php
@@ -4,7 +4,6 @@ namespace FactoryGirl\Tests\Provider\Doctrine\Types;
 
 use FactoryGirl\Tests\Provider\Doctrine\Fixtures\TestCase;
 use Doctrine\DBAL\Types\Type;
-use Doctrine\Tests\DBAL\Mocks\MockPlatform;
 
 Type::addType('statusarray', 'FactoryGirl\Provider\Doctrine\DBAL\Types\StatusArrayType');
 
@@ -126,5 +125,49 @@ class StatusArrayTest extends TestCase
     }
     
     
+}
 
+
+class MockPlatform extends \Doctrine\DBAL\Platforms\AbstractPlatform
+{
+    /**
+     * Gets the SQL Snippet used to declare a BLOB column type.
+     */
+    public function getBlobTypeDeclarationSQL(array $field)
+    {
+        throw DBALException::notSupported(__METHOD__);
+    }
+
+    public function getBooleanTypeDeclarationSQL(array $columnDef) {}
+    public function getIntegerTypeDeclarationSQL(array $columnDef) {}
+    public function getBigIntTypeDeclarationSQL(array $columnDef) {}
+    public function getSmallIntTypeDeclarationSQL(array $columnDef) {}
+    public function _getCommonIntegerTypeDeclarationSQL(array $columnDef) {}
+
+    public function getVarcharTypeDeclarationSQL(array $field)
+    {
+        return "DUMMYVARCHAR()";
+    }
+
+    /** @override */
+    public function getClobTypeDeclarationSQL(array $field)
+    {
+        return 'DUMMYCLOB';
+    }
+
+    public function getVarcharDefaultLength()
+    {
+        return 255;
+    }
+
+    public function getName()
+    {
+        return 'mock';
+    }
+    protected function initializeDoctrineTypeMappings() {
+    }
+    protected function getVarcharTypeDeclarationSQLSnippet($length, $fixed)
+    {
+
+    }
 }
