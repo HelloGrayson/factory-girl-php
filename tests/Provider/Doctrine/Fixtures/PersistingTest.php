16a66d918ac2a65986cb5182f5278ab9d675ea35
Jakub Pavlík <severus@post.cz>
2017-12-29 14:57:06 +0100

    extract shared definition

--- tests/Provider/Doctrine/Fixtures/PersistingTest.php
+++ tests/Provider/Doctrine/Fixtures/PersistingTest.php
@@ -3,30 +3,35 @@ namespace FactoryGirl\Tests\Provider\Doctrine\Fixtures;
 
 class PersistingTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->factory->defineEntity('SpaceShip', array('name' => 'Zeta'));
+    }
+
     /**
      * @test
      */
     public function automaticPersistCanBeTurnedOn()
     {
-        $this->factory->defineEntity('SpaceShip', array('name' => 'Zeta'));
-        
         $this->factory->persistOnGet();
+
         $ss = $this->factory->get('SpaceShip');
         $this->em->flush();
-        
+
         $this->assertNotNull($ss->getId());
         $this->assertTrue($this->em->contains($ss));
     }
-    
+
     /**
      * @test
      */
     public function doesNotPersistByDefault()
     {
-        $this->factory->defineEntity('SpaceShip', array('name' => 'Zeta'));
         $ss = $this->factory->get('SpaceShip');
         $this->em->flush();
-        
+
         $this->assertNull($ss->getId());
         $this->assertFalse($this->em->contains($ss));
     }
